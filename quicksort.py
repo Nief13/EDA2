5029bb335f84bb1b2b840271009f1a8e7ff5094e
Vinícius da CostaFerreira <vinicius.c.f@outlook.com>
2018-03-30 19:17:16 -0300

    Add files via upload

---
+++ quicksort.py
@@ -0,0 +1,21 @@
+import numpy as np
+import random
+
+tamanho=10
+tempos=[]
+tamanhos=[]
+#def quicksort (vetor):
+
+#	return tempo;
+	
+while(tamanho<=100000):
+	vetor = []
+	for _ in range(tamanho):
+		vetor.append(random.randint(0, 100))
+	#tempo=quicksort(vetor)
+	del vetor [ 00:len(vetor) ] 
+	print(tamanho)
+	if(tamanho>=1000):
+		tamanho=tamanho+1000
+	else:
+		tamanho=tamanho*10
